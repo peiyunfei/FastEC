2560ea4ee7ee636a5d98b5368f5db98f74e3de51
peiyunfei <1027282775@qq.com>
2017-07-10 20:21:18 +0800

    基于Retrofit的高性能网络请求框架封装——封装http请求接口。

---
+++ latte-core/src/main/java/com/pyf/latte/net/RestService.java
@@ -0,0 +1,104 @@
+package com.pyf.latte.net;
+
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
+/**
+ * http请求接口
+ * <br/>
+ * 作者：裴云飞
+ * <br/>
+ * 时间：2017/7/10
+ */
+public interface RestService {
+
+    /**
+     * get请求
+     *
+     * @param url
+     *         请求地址
+     * @param params
+     *         请求参数
+     * @return call实例，通过回调获取服务器返回的数据
+     */
+    @GET
+    Call<String> get(@Url String url, @QueryMap Map<String, Object> params);
+
+    /**
+     * post请求
+     *
+     * @param url
+     *         请求地址
+     * @param params
+     *         请求参数
+     * @return call实例，通过回调获取服务器返回的数据
+     */
+    @FormUrlEncoded
+    @POST
+    Call<String> post(@Url String url, @FieldMap Map<String, Object> params);
+
+    /**
+     * put请求
+     *
+     * @param url
+     *         请求地址
+     * @param params
+     *         请求参数
+     * @return call实例，通过回调获取服务器返回的数据
+     */
+    @FormUrlEncoded
+    @PUT
+    Call<String> put(@Url String url, @FieldMap Map<String, Object> params);
+
+    /**
+     * delete请求
+     *
+     * @param url
+     *         请求地址
+     * @param params
+     *         请求参数
+     * @return call实例，通过回调获取服务器返回的数据
+     */
+    @DELETE
+    Call<String> delete(@Url String url, @QueryMap Map<String, Object> params);
+
+    /**
+     * 下载请求
+     *
+     * @param url
+     *         请求地址
+     * @param params
+     *         请求参数
+     * @return call实例，通过回调获取服务器返回的数据
+     */
+    @Streaming
+    @GET
+    Call<RequestBody> download(@Url String url, @QueryMap Map<String, Object> params);
+
+    /**
+     * 上传文件请求
+     *
+     * @param url
+     *         请求地址
+     * @param file
+     *         要上传的文件
+     * @return call实例，通过回调获取服务器返回的数据
+     */
+    @Multipart
+    @POST
+    Call<String> upload(@Url String url, @Part MultipartBody file);
+}
